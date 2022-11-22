82acb7cb30c0e265f67afd7c60c144b419492e25
relictgit <95646932+relictgit@users.noreply.github.com>
2022-11-22 22:56:06 +0100

    Update main.c

--- password_generaor/main.c
+++ password_generaor/main.c
@@ -3,14 +3,6 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-  /* ToDo
-   * 1. generate a random amount of characters.
-   * 2. it must be a combination of digits, lower and upper case letters and
-   * other characters
-   * 3. the maximum number of characters should be considered as to be chosen by
-   * the user
-   */
-
   int passwd_lenght = 0;
 
   printf("Enter a length for the password that needs to be provided!\nYour "
