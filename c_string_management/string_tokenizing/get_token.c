cf1e5438af092af0f83e7b3acf280d3d3cb72ce4
Kvoss <kvoss_git@freenet.de>
2022-11-23 23:52:04 +0100

    added a call by value and a call by referenz method

--- c_string_management/string_tokenizing/get_token.c
+++ c_string_management/string_tokenizing/get_token.c
@@ -3,27 +3,45 @@
 #include <stdlib.h>
 #include <string.h>
 
+void get_specific_token_referenze(char string[], char delimeter[], int position,
+                                  char **string_token) {
+  *string_token = strtok(string, delimeter);
+  if (*string_token == NULL) {
+    char *error = "There is nothing to tokenize!\n";
+    printf("%s", error);
+    exit(EXIT_FAILURE);
+  }
+  for (int counter = 1; counter < position; ++counter) {
+    *string_token = strtok(NULL, delimeter);
+  }
+}
+
 char *get_specific_token(char string[], char delimeter[], int position) {
   char *string_token = NULL;
   string_token = strtok(string, delimeter);
-  int counter = 1;
-  while (string_token != NULL) {
-    if (counter == position) {
-      break;
-    }
+  if (string_token == NULL) {
+    char *error = "There is nothing to tokenize!\n";
+    printf("%s", error);
+    return error;
+  }
+  for (int counter = 1; counter < position; ++counter) {
     string_token = strtok(NULL, delimeter);
-    ++counter;
   }
   return string_token;
 }
 
 void tokenize_string() {
-  char string[] = "This Text is Token 1:This text is Token2:Token3";
+  char string1[] = "This Text is Token 1:This text is Token2:Token3";
+  char string2[] =
+      "van-harem:Token:diddo:karlos:hamburger:good-old-crusty-crab";
   printf("lets start with a easy task tokenizing a string\n");
-  printf("string: %s\n", string);
+  printf("string1: %s\nstring2: %s\n", string1, string2);
   char *string_token = NULL;
   char delimeter[] = ":";
-  int position = 3;
-  printf("string tokenised by position 3: %s\n",
-         get_specific_token(string, delimeter, position));
+  int position = 2;
+  printf("string1 tokenised by position %d\n and by call by value: %s\n",
+         position, get_specific_token(string1, delimeter, position));
+  get_specific_token_referenze(string2, delimeter, position, &string_token);
+  printf("string2 tokenized by position: %d\ncall by referenze output: %s\n",
+         position, string_token);
 }
--- c_string_management/string_tokenizing/get_token.h
+++ c_string_management/string_tokenizing/get_token.h
@@ -3,5 +3,7 @@
 
 void tokenize_string();
 char *get_specific_token(char string[], char delimeter[], int position);
+void get_specific_token_referenze(char string[], char delimeter[], int position,
+                                  char **string_token);
 
 #endif // GET_TOKEN_H
