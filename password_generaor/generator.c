ed3cf7d091666556b263558e691bf22140a79e9c
Kvoss <kvoss_git@freenet.de>
2022-11-23 20:37:03 +0100

    small improvements to the code

--- password_generaor/generator.c
+++ password_generaor/generator.c
@@ -7,41 +7,53 @@
 void generator(int passwd_len) {
   // prerequisite initializations
   int randomizer = 0;
-  short num_selectors = 4;
+  short num_selectors = 0;
+  short max_digit_num = 10;
+  short max_symbol_num = 10;
+  short max_letter_num = 26;
 
   // seed the random number generator
   // with the current time so that the
   // number will be different every time
   srand((unsigned int)(time(NULL)));
 
-  /* char sets of the characters to choose from */
+  // char sets of the characters to choose from
+  // note: char arrays are recommended to be set to a fixed size
   char digits[10] = "0123456789";
+  ++num_selectors;
   char letters_lower[26] = "abcdefghijklmnopqrstuvwxyz";
+  ++num_selectors;
   char letters_upper[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  ++num_selectors;
   char symbols[10] = "!@_-$^&*%?";
+  ++num_selectors;
+  // printf("-----debug-----\nselector number: %i\n", num_selectors);
 
+  // initialization of the password variable
   char passwd[passwd_len];
 
   // to select the randomizer inside the loop
   randomizer = rand() % num_selectors;
 
+  // build up the password string and print it as the output onto the screen
   for (int counter = 0; counter < passwd_len; ++counter) {
     if (randomizer == 1) {
-      passwd[counter] = digits[rand() % 10];
+      passwd[counter] = digits[rand() % max_digit_num];
       randomizer = rand() % num_selectors;
       printf("%c", passwd[counter]);
     } else if (randomizer == 2) {
-      passwd[counter] = letters_lower[rand() % 26];
+      passwd[counter] = letters_lower[rand() % max_letter_num];
       randomizer = rand() % num_selectors;
       printf("%c", passwd[counter]);
     } else if (randomizer == 3) {
-      passwd[counter] = letters_upper[rand() % 26];
+      passwd[counter] = letters_upper[rand() % max_letter_num];
       randomizer = rand() % num_selectors;
       printf("%c", passwd[counter]);
     } else {
-      passwd[counter] = symbols[rand() % 10];
+      passwd[counter] = symbols[rand() % max_symbol_num];
       randomizer = rand() % num_selectors;
       printf("%c", passwd[counter]);
     }
   }
+  printf("\n");
 }
--- password_generaor/main.c
+++ password_generaor/main.c
@@ -5,11 +5,21 @@
 int main(int argc, char *argv[]) {
   int passwd_lenght = 0;
 
-  printf("Enter a length for the password that needs to be provided!\nYour "
+  /* printf("Enter a length for the password that needs to be provided!\nYour "
          "Passwd length: ");
-  scanf("%d", &passwd_lenght);
+  scanf("%d", &passwd_lenght); */
 
-  generator(passwd_lenght);
-
-  return EXIT_SUCCESS;
+  if (argc == 2) {
+    passwd_lenght = atoi(argv[1]);
+    generator(passwd_lenght);
+    return EXIT_SUCCESS;
+  } else {
+    fprintf(stderr,
+            "----------PROBLEM----------\n"
+            "arguments passed: %i\n"
+            "arguments needed: 2\n"
+            "Usage: %s <password_length>\n",
+            argc, argv[0]);
+    return EXIT_FAILURE;
+  }
 }
