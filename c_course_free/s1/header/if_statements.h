913a3b7218d0266baec057c08dc9f674b48cff8f
Kvoss <kvoss_git@freenet.de>
2023-10-23 19:14:03 +0200

    added simple example demonstration for the if () {}, else if () {} and else {} statemets

---
+++ c_course_free/s1/header/if_statements.h
@@ -0,0 +1,6 @@
+#ifndef IF_STATEMENTS_H
+#define IF_STATEMENTS_H
+
+short example_if_statements();
+
+#endif // IF_STATEMENTS_H
---
+++ c_course_free/s1/src/if_statements.c
@@ -0,0 +1,20 @@
+#include "../header/if_statements.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+short example_if_statements() {
+  int age = 0;
+
+  printf("Enter your age: ");
+  scanf("%d", &age);
+
+  if (age >= 18) {
+    printf("You are now signed up!\n");
+  } else if (age < 0) {
+    printf("You have not been born yet!\n");
+  } else {
+    printf("You are too young to sign up!\n");
+  }
+
+  return 0;
+}
