8a6ccac29f250923f3f5cd838e0c66cafa17df25
Kvoss <kvoss_git@freenet.de>
2023-10-24 19:10:26 +0200

    added argument examples on functions

---
+++ c_course_free/s1/header/arguments.h
@@ -0,0 +1,7 @@
+#ifndef ARGUMENTS_H
+#define ARGUMENTS_H
+
+void print_birthday(char name[], char age);
+short function_for_printing_happy_birthday_with_parameter_fn();
+
+#endif // ARGUMENTS_H
--- c_course_free/s1/src/main.c
+++ c_course_free/s1/src/main.c
@@ -1,9 +1,7 @@
-#include "../header/calculator.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 int main() {
   printf("Hello World!\n");
-  example_calculator();
   return 0;
 }
