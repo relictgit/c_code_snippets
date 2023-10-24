dd5ddc1710f8ba846f4c127d52cf966f7e753dd3
Kvoss <kvoss_git@freenet.de>
2023-10-24 19:01:27 +0200

    added basic function how to

---
+++ c_course_free/s1/header/functions.h
@@ -0,0 +1,7 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+void print_happy_birthday();
+short function();
+
+#endif // FUNCTIONS_H
---
+++ c_course_free/s1/src/functinos.c
@@ -0,0 +1,11 @@
+#include "../header/functions.h"
+#include <stdio.h>
+
+// here only the bare basics of functions
+
+void print_happy_birthday() { printf("happy birthday... dear you\n"); }
+
+short function() {
+  print_happy_birthday();
+  return 0;
+}
