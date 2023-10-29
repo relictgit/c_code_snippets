0aa9413657cdf12bd11af109bc532bb245f711fe
Kvoss <kvoss_git@freenet.de>
2023-10-29 16:25:17 +0100

    added simple example for array of strings

---
+++ c_course_free/s1/header/array_of_strings.h
@@ -0,0 +1,6 @@
+#ifndef ARRAY_OF_STRINGS_H
+#define ARRAY_OF_STRINGS_H
+
+short main_array_of_strings();
+
+#endif // ARRAY_OF_STRINGS_H
---
+++ c_course_free/s1/src/array_of_strings.c
@@ -0,0 +1,16 @@
+#include "../header/array_of_strings.h"
+#include <stdio.h>
+#include <string.h>
+
+short main_array_of_strings() {
+  char cars[][10] = {"Mustang", "Audi", "Lada"};
+
+  // cars[0] = "Tesla";
+  strcpy(cars[0], "Tesla");
+
+  for (int i = 0; i < sizeof(cars) / sizeof(cars[0]); i++) {
+    printf("%s\n", cars[i]);
+  }
+
+  return 0;
+}
