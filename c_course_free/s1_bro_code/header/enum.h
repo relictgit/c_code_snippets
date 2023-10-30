550ab267bfc1f1859b36708cfdf18c0eb744e756
Kvoss <kvoss_git@freenet.de>
2023-10-30 18:47:56 +0100

    added simple example of enums

---
+++ c_course_free/s1_bro_code/header/enum.h
@@ -0,0 +1,8 @@
+#ifndef ENUM_H
+#define ENUM_H
+
+enum day { Sun, Mon, Tue, Wed, Thu, Fri, Sat };
+
+short main_enum();
+
+#endif // ENUM_H
---
+++ c_course_free/s1_bro_code/src/enum.c
@@ -0,0 +1,19 @@
+#include "../header/enum.h"
+#include <stdio.h>
+
+short main_enum() {
+  // enum = a user defined type of named integer identifiers
+  //        helps to make a program more readable
+
+  enum day today = Sun;
+
+  // printf("today is: %d\n", today);
+
+  if (/*today == 0 || today == 6*/ today == Sun || today == Sat) {
+    printf("It is weekend.\n");
+  } else {
+    printf("I have to work today...\n");
+  }
+
+  return 0;
+}
