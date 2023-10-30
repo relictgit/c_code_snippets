ce80ee50ddc705a208d359f843e5c1ff68cb741b
Kvoss <kvoss_git@freenet.de>
2023-10-30 19:09:19 +0100

    added a simple example for a guessing game

---
+++ c_course_free/s1_bro_code/header/guessing_game.h
@@ -0,0 +1,6 @@
+#ifndef GUESSING_GAME_H
+#define GUESSING_GAME_H
+
+short main_guessing_game();
+
+#endif // GUESSING_GAME_H
---
+++ c_course_free/s1_bro_code/src/guessing_game.c
@@ -0,0 +1,40 @@
+#include "../header/guessing_game.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+short main_guessing_game() {
+  const int MIN = 1;
+  const int MAX = 100;
+  int guess = 0;
+  int guesses = 0;
+  int answer = 0;
+
+  // uses current time as a seed
+  srand(time(0));
+
+  // generate a rand number between MIN and MAX
+  answer = (rand() % MAX) + MIN;
+
+  /* printf("answer is: %d\n", answer); */
+
+  do {
+    printf("Ener a guess: ");
+    scanf("%d", &guess);
+    if (guess > answer) {
+      printf("guess: %d --- is to high\n", guess);
+    } else if (guess < answer) {
+      printf("guess: %d --- is to low\n", guess);
+    } else {
+      printf("CORRECT!\n");
+    }
+    guesses++;
+  } while (guess != answer);
+
+  printf("===========================================\n");
+  printf("answer: %d\n", answer);
+  printf("guesses: %d\n", guesses);
+  printf("===========================================\n");
+
+  return 0;
+}
