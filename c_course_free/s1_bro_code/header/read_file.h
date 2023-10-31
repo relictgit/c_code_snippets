78d911be6c0da77d0a3f903ae9495677c38a2d06
Kvoss <kvoss_git@freenet.de>
2023-10-31 19:34:04 +0100

    added example of reading from a file

---
+++ c_course_free/s1_bro_code/header/read_file.h
@@ -0,0 +1,6 @@
+#ifndef READ_FILE_H
+#define READ_FILE_H
+
+short main_read_file();
+
+#endif // READ_FILE_H
---
+++ c_course_free/s1_bro_code/src/read_file.c
@@ -0,0 +1,24 @@
+#include "../header/read_file.h"
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+short main_read_file() {
+  FILE *fptr = NULL;
+  char buffer[300] = "";
+  int buffer_size = sizeof(buffer) / sizeof(buffer[0]);
+
+  if ((fptr = fopen("test.txt", "r")) == NULL) {
+    fprintf(stderr, "Could not open file!\nErrorcode is: %s\n",
+            strerror(errno));
+    exit(EXIT_FAILURE);
+  } else {
+    while ((fgets(buffer, buffer_size, fptr)) != NULL) {
+      printf("%s\n", buffer);
+    }
+    fclose(fptr);
+  }
+
+  return 0;
+}
