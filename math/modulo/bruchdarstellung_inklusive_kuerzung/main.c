38631fe89157f131ce528e44635f7542faab87eb
Kvoss <kvoss_git@freenet.de>
2024-02-23 23:49:36 +0100

    changed tabstops

--- math/modulo/bruchdarstellung_inklusive_kuerzung/main.c
+++ math/modulo/bruchdarstellung_inklusive_kuerzung/main.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (int argc, char *argv[]) {
-    if (argc != 3) {
-        printf ("Usage: %s <bruch_zaehler> <bruch_nenner>\n", argv[0]);
-        exit (EXIT_FAILURE);
+int main(int argc, char *argv[]) {
+    if(argc != 3) {
+        printf("Usage: %s <bruch_zaehler> <bruch_nenner>\n", argv[0]);
+        exit(EXIT_FAILURE);
     }
 
-    int bruch_zaehler = atoi (argv[1]);
-    int bruch_nenner = atoi (argv[2]);
+    int bruch_zaehler = atoi(argv[1]);
+    int bruch_nenner = atoi(argv[2]);
     short count_back;
     const short MIMINAL_DIV = 2;
     const short ZERO = 0;
 
-    printf ("now showing the calculation steps\n");
-    printf ("================================================\n");
+    printf("now showing the calculation steps\n");
+    printf("================================================\n");
 
-    for (count_back = 100; count_back >= MIMINAL_DIV; count_back--) {
-        while (bruch_zaehler % count_back == ZERO && bruch_nenner % count_back == ZERO) {
-            printf (
+    for(count_back = 100; count_back >= MIMINAL_DIV; count_back--) {
+        while(bruch_zaehler % count_back == ZERO && bruch_nenner % count_back == ZERO) {
+            printf(
                 "bruch_zaehler: %d\t\tbruch_nenner: %d\t\tdivision by: %d\n", bruch_zaehler,
                 bruch_nenner, count_back);
             bruch_zaehler /= count_back;
@@ -26,9 +26,9 @@ int main (int argc, char *argv[]) {
         }
     }
 
-    printf ("\nnow showing the result\n");
-    printf ("================================================\n");
-    printf ("Bruch = %d/%d\n", bruch_zaehler, bruch_nenner);
+    printf("\nnow showing the result\n");
+    printf("================================================\n");
+    printf("Bruch = %d/%d\n", bruch_zaehler, bruch_nenner);
 
     return 0;
 }
