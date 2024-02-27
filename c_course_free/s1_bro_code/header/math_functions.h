95a339729e941750fac1a95f02fdf412aa376ff6
Kvoss <kvoss_git@freenet.de>
2024-02-27 07:39:54 +0100

    added the math_functions.h file

---
+++ c_course_free/s1_bro_code/header/math_functions.h
@@ -0,0 +1,19 @@
+#ifndef MATH_FUNCTIONS_H
+#define MATH_FUNCTIONS_H
+
+typedef struct {
+    double sqrt;
+    double pow;
+    int round;
+    int ceil;
+    int floor;
+    int abs;
+    double log;
+    double sin;
+    double cos;
+    double tan;
+} maths_t;
+
+int math_functions();
+
+#endif // MATH_FUNCTIONS_H
